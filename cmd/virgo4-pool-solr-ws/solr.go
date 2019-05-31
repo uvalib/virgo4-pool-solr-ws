df375a14ddd8e8bd7cab21224c1f1c929f0f19ec
Jason Jordan <jlj5aj@virginia.edu>
2019-05-31 16:50:05 -0400

    add query elapsed time logging to debug spikes

--- cmd/virgo4-pool-solr-ws/solr.go
+++ cmd/virgo4-pool-solr-ws/solr.go
@@ -36,11 +36,17 @@ func solrQuery(solrReq solrRequest) (*solrResponse, error) {
 
 	log.Printf("solr req: [%s]", req.URL.String())
 
+	start := time.Now()
+
 	res, resErr := solrClient.Do(req)
 	if resErr != nil {
 		log.Printf("client.Do() failed: %s", resErr.Error())
 		return nil, errors.New("Failed to receive Solr response")
 	}
+
+	elapsed := time.Since(start).Seconds()
+
+	log.Printf("solr query elapsed time: %0.3fs", elapsed)
 
 	defer res.Body.Close()
 
@@ -49,13 +55,13 @@ func solrQuery(solrReq solrRequest) (*solrResponse, error) {
 	var solrRes solrResponse
 
 	decoder := json.NewDecoder(res.Body)
-	decErr := decoder.Decode(&solrRes)
-	if decErr != nil {
+
+	if decErr := decoder.Decode(&solrRes); decErr != nil {
 		log.Printf("Decode() failed: %s", decErr.Error())
 		return nil, errors.New("Failed to decode Solr response")
 	}
 
-	logHeader := fmt.Sprintf("solr res: header: { status = %d, QTime = %d }", solrRes.ResponseHeader.Status, solrRes.ResponseHeader.QTime)
+	logHeader := fmt.Sprintf("solr res: header: { status = %d, QTime = %d (elapsed: %0.3fs) }", solrRes.ResponseHeader.Status, solrRes.ResponseHeader.QTime, elapsed)
 
 	// quick validation
 	if solrRes.ResponseHeader.Status != 0 {
