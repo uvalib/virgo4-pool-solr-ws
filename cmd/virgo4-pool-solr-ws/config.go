76f6eaf72928181c80a5f7791d6719b78dc2d8c3
Jason Jordan <jlj5aj@virginia.edu>
2019-08-06 13:47:45 -0400

    add score threshold vars; ensure vars are set, and in most cases, nonempty

--- cmd/virgo4-pool-solr-ws/config.go
+++ cmd/virgo4-pool-solr-ws/config.go
@@ -21,24 +21,50 @@ type poolConfig struct {
 	solrParameterFq      string
 	solrParameterFl      string
 	solrAvailableFacets  string
+	scoreThresholdMedium string
+	scoreThresholdHigh   string
+}
+
+func ensureSet(env string) string {
+	val, set := os.LookupEnv(env)
+
+	if set == false {
+		log.Printf("environment variable not set: [%s]", env)
+		os.Exit(1)
+	}
+
+	return val
+}
+
+func ensureSetAndNonEmpty(env string) string {
+	val := ensureSet(env)
+
+	if val == "" {
+		log.Printf("environment variable not set: [%s]", env)
+		os.Exit(1)
+	}
+
+	return val
 }
 
 func (cfg *poolConfig) load() {
-	cfg.listenPort = os.Getenv("VIRGO4_SOLR_POOL_WS_LISTEN_PORT")
-	cfg.poolType = os.Getenv("VIRGO4_SOLR_POOL_WS_POOL_TYPE")
-	cfg.poolDescription = os.Getenv("VIRGO4_SOLR_POOL_WS_POOL_DESCRIPTION")
-	cfg.poolServiceURL = os.Getenv("VIRGO4_SOLR_POOL_WS_POOL_SERVICE_URL")
-	cfg.poolLeaders = os.Getenv("VIRGO4_SOLR_POOL_WS_POOL_LEADERS")
-	cfg.solrHost = os.Getenv("VIRGO4_SOLR_POOL_WS_SOLR_HOST")
-	cfg.solrCore = os.Getenv("VIRGO4_SOLR_POOL_WS_SOLR_CORE")
-	cfg.solrHandler = os.Getenv("VIRGO4_SOLR_POOL_WS_SOLR_HANDLER")
-	cfg.solrConnTimeout = os.Getenv("VIRGO4_SOLR_POOL_WS_SOLR_CONN_TIMEOUT")
-	cfg.solrReadTimeout = os.Getenv("VIRGO4_SOLR_POOL_WS_SOLR_READ_TIMEOUT")
-	cfg.solrParameterQt = os.Getenv("VIRGO4_SOLR_POOL_WS_SOLR_PARAMETER_QT")
-	cfg.solrParameterDefType = os.Getenv("VIRGO4_SOLR_POOL_WS_SOLR_PARAMETER_DEFTYPE")
-	cfg.solrParameterFq = os.Getenv("VIRGO4_SOLR_POOL_WS_SOLR_PARAMETER_FQ")
-	cfg.solrParameterFl = os.Getenv("VIRGO4_SOLR_POOL_WS_SOLR_PARAMETER_FL")
-	cfg.solrAvailableFacets = os.Getenv("VIRGO4_SOLR_POOL_WS_SOLR_AVAILABLE_FACETS")
+	cfg.listenPort = ensureSetAndNonEmpty("VIRGO4_SOLR_POOL_WS_LISTEN_PORT")
+	cfg.poolType = ensureSetAndNonEmpty("VIRGO4_SOLR_POOL_WS_POOL_TYPE")
+	cfg.poolDescription = ensureSetAndNonEmpty("VIRGO4_SOLR_POOL_WS_POOL_DESCRIPTION")
+	cfg.poolServiceURL = ensureSetAndNonEmpty("VIRGO4_SOLR_POOL_WS_POOL_SERVICE_URL")
+	cfg.poolLeaders = ensureSet("VIRGO4_SOLR_POOL_WS_POOL_LEADERS")
+	cfg.solrHost = ensureSetAndNonEmpty("VIRGO4_SOLR_POOL_WS_SOLR_HOST")
+	cfg.solrCore = ensureSetAndNonEmpty("VIRGO4_SOLR_POOL_WS_SOLR_CORE")
+	cfg.solrHandler = ensureSetAndNonEmpty("VIRGO4_SOLR_POOL_WS_SOLR_HANDLER")
+	cfg.solrConnTimeout = ensureSetAndNonEmpty("VIRGO4_SOLR_POOL_WS_SOLR_CONN_TIMEOUT")
+	cfg.solrReadTimeout = ensureSetAndNonEmpty("VIRGO4_SOLR_POOL_WS_SOLR_READ_TIMEOUT")
+	cfg.solrParameterQt = ensureSet("VIRGO4_SOLR_POOL_WS_SOLR_PARAMETER_QT")
+	cfg.solrParameterDefType = ensureSetAndNonEmpty("VIRGO4_SOLR_POOL_WS_SOLR_PARAMETER_DEFTYPE")
+	cfg.solrParameterFq = ensureSet("VIRGO4_SOLR_POOL_WS_SOLR_PARAMETER_FQ")
+	cfg.solrParameterFl = ensureSetAndNonEmpty("VIRGO4_SOLR_POOL_WS_SOLR_PARAMETER_FL")
+	cfg.solrAvailableFacets = ensureSetAndNonEmpty("VIRGO4_SOLR_POOL_WS_SOLR_AVAILABLE_FACETS")
+	cfg.scoreThresholdMedium = ensureSetAndNonEmpty("VIRGO4_SOLR_POOL_WS_SCORE_THRESHOLD_MEDIUM")
+	cfg.scoreThresholdHigh = ensureSetAndNonEmpty("VIRGO4_SOLR_POOL_WS_SCORE_THRESHOLD_HIGH")
 
 	log.Printf("[CONFIG] listenPort           = [%s]", cfg.listenPort)
 	log.Printf("[CONFIG] poolType             = [%s]", cfg.poolType)
@@ -55,4 +81,6 @@ func (cfg *poolConfig) load() {
 	log.Printf("[CONFIG] solrParameterFq      = [%s]", cfg.solrParameterFq)
 	log.Printf("[CONFIG] solrParameterFl      = [%s]", cfg.solrParameterFl)
 	log.Printf("[CONFIG] solrAvailableFacets  = [%s]", cfg.solrAvailableFacets)
+	log.Printf("[CONFIG] scoreThresholdMedium = [%s]", cfg.scoreThresholdMedium)
+	log.Printf("[CONFIG] scoreThresholdHigh   = [%s]", cfg.scoreThresholdHigh)
 }
