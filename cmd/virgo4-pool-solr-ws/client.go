448b6596d73d9198e3c10ff39ed23b2b5ee879cd
Jason Jordan <jlj5aj@virginia.edu>
2019-09-17 19:09:17 -0400

    default to grouped records

--- cmd/virgo4-pool-solr-ws/client.go
+++ cmd/virgo4-pool-solr-ws/client.go
@@ -74,7 +74,7 @@ func (c *clientContext) init(p *poolContext, ctx *gin.Context) {
 	c.opts.debug = boolOptionWithFallback(ctx.Query("debug"), false)
 	c.opts.intuit = boolOptionWithFallback(ctx.Query("intuit"), true)
 	c.opts.verbose = boolOptionWithFallback(ctx.Query("verbose"), false)
-	c.opts.grouped = boolOptionWithFallback(ctx.Query("grouped"), false)
+	c.opts.grouped = boolOptionWithFallback(ctx.Query("grouped"), true)
 
 	query := ""
 	if ctx.Request.URL.RawQuery != "" {
