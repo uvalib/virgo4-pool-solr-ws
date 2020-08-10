ccf29f227bce2237dd99f45c82009aa27674f7b9
Jason Jordan <jlj5aj@virginia.edu>
2020-08-10 13:21:09 -0400

    VIRGONEW-1598: better categorize unexpected errors

--- cmd/pdf.go
+++ cmd/pdf.go
@@ -33,16 +33,16 @@ func (s *searchContext) getPdfStatus(url string) (string, error) {
 		}
 
 		s.log("[PDF] client.Do() failed: %s", resErr.Error())
-		s.log("WARNING: Failed response from %s %s - %d:%s. Elapsed Time: %d (ms)", req.Method, url, status, errMsg, elapsedMS)
+		s.log("ERROR: Failed response from %s %s - %d:%s. Elapsed Time: %d (ms)", req.Method, url, status, errMsg, elapsedMS)
 		return "", fmt.Errorf("failed to receive PDF status response")
 	}
 
 	defer res.Body.Close()
 
-	if res.StatusCode != http.StatusOK {
+	if res.StatusCode != http.StatusOK && res.StatusCode != http.StatusNotFound {
 		errMsg := fmt.Errorf("unexpected status code %d", res.StatusCode)
 		s.log("[PDF] unexpected status code %d", res.StatusCode)
-		s.log("WARNING: Failed response from %s %s - %d:%s. Elapsed Time: %d (ms)", req.Method, url, res.StatusCode, errMsg, elapsedMS)
+		s.log("ERROR: Failed response from %s %s - %d:%s. Elapsed Time: %d (ms)", req.Method, url, res.StatusCode, errMsg, elapsedMS)
 		return "", fmt.Errorf("received PDF status response code %d", res.StatusCode)
 	}
 
